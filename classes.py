d571feb6a859f94416adc71dc20a873de98fd945
argentavis <thepalewhitegamer@gmail.com>
2023-03-30 19:17:01 -0400

    species classes can now init from csv

--- classes.py
+++ classes.py
@@ -1,16 +1,24 @@
 from enum import Enum
+from typing import Optional
+from dataclasses import dataclass, field
 
 """
 ,pokedex_number,name,,,generation,status,species,
-type_number,type_1,type_2,height_m,weight_kg,
-abilities_number,ability_1,ability_2,ability_hidden,
-total_points,hp,attack,defense,sp_attack,sp_defense,speed,
+,type_1,type_2,height_m,weight_kg,
+,ability_1,ability_2,ability_hidden,
+,hp,attack,defense,sp_attack,sp_defense,speed,
 catch_rate,base_friendship,base_experience,growth_rate,
 ,,,,,
 ,,,,,,
 ,,,,,,
 ,,,,,
 
+"""
+
+"""
+https://www.kaggle.com/datasets/mariotormo/complete-pokemon-dataset-updated-090420?resource=download&select=pokedex_%28Update_05.20%29.csv
+
+thanks mario
 """
 
 type_names = [
@@ -47,7 +55,7 @@ class Species():
     pokedex_species : str
     
     type_1 : Elemental_Type
-    type_2 : Elemental_Type
+    type_2 : Optional[Elemental_Type]
     
     height_m : float
     weight_kg : float
@@ -56,7 +64,6 @@ class Species():
     ability_2 : str
     ability_hidden : str
     
-    bst : int
     hp : int
     attack : int
     defense : int
@@ -69,8 +76,62 @@ class Species():
     base_experience : int # ?
     growth_rate : str
     
+    def get_bst(self):
+        return self.hp + self.attack + self.defense + self.sp_attack + self.sp_defense + self.speed
     
     @classmethod
-    def from_file(cls, file_path):
-        "Create a pokemon from a csv."
-        return Person('John', 'Doe', 25)
+    def from_file(cls, pokedex_number, file_path = "data/pokedex.csv"):
+        """
+        Load a pokemon species from a csv file.
+        
+        This is done by simply looping through the list until we find the right natdexno.
+        """
+        with open(file_path, "r+", encoding="utf-8") as f:
+            
+            for dexentry in f.read().splitlines():
+                d = dexentry.split(",")
+                
+                if str(pokedex_number) == d[1]:
+                    return Species(
+                    pokedex_number  = d[1],
+                    name            = d[2],
+                    generation      = d[5],
+                    status          = d[6],
+                    pokedex_species = d[7],
+                    
+                    type_1          = Elemental_Type[d[9].lower()],
+                    type_2          = Elemental_Type[d[10].lower()] if d[10] else None,
+                    
+                    height_m        = d[11],
+                    weight_kg       = d[12],
+                    
+                    ability_1       = d[14],
+                    ability_2       = d[15],
+                    ability_hidden  = d[16],
+                    
+                    hp              = int(d[18]),
+                    attack          = int(d[19]),
+                    defense         = int(d[20]),
+                    sp_attack       = int(d[21]),
+                    sp_defense      = int(d[22]),
+                    speed           = int(d[23]),
+                    
+                    catch_rate      = d[24],
+                    base_friendship = d[25],
+                    base_experience = d[26],
+                    growth_rate     = d[27]
+                    )
+
+
+if __name__ == "__main__":
+    print(Species.from_file(25)  )
+    print()
+    print(Species.from_file(151) )
+    print()
+    print(Species.from_file(251) )
+    print()
+    print(Species.from_file(386) )
+    print()
+    print(Species.from_file(493) )
+    print()
+    print(Species.from_file(649) )
